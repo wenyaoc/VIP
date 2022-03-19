68fa311b7ed2dcd4c6058b7cb32ec65061e383b1
Shaoqian Zhou <z5244813@student.unsw.edu.au>
2022-03-19 14:18:38 +1100

    fft plot

--- fft_plot.py
+++ fft_plot.py
@@ -4,33 +4,38 @@ from scipy.fftpack import fft, ifft
 import pandas as pd
 
 # Import csv file
-df = pd.read_csv('rsam_2016-17_fft_test.csv', index_col=['DateTime'], parse_dates=['DateTime'])
-print(df.head())
+# df = pd.read_csv('minzhao.csv', index_col=['TIME'], parse_dates=['TIME'])
+# print(df.head())
 
-#plot data
-plt.figure(figsize=(12,4))
-df.plot(linestyle = '', marker = '*', color='r')
-plt.savefig('rsam_2016_2017_snippetforfft.jpg')
-plt.show()
 
-#FFT
-#number of sample points
-N = 407
-#frequency of signal (in days)
+
+# plot data
+#plt.figure(figsize=(12,4))
+#df.plot(linestyle = '', marker = '*', color='r')
+#plt.savefig('rsam_2016_2017_snippetforfft.jpg')
+#plt.show()
+
+# FFT
+# number of sample points
+import packet_rate_fft
+
+N = 62
+# frequency of signal (in second)
 T = 1
-#create x-axis for time length of signal
+# create x-axis for time length of signal
 x = np.linspace(0, N*T, N)
-#create array that corresponds to values in signal
-y = df
-#perform FFT on signal
-yf = fft(y)
-#create new x-axis: frequency from signal
+# create array that corresponds to values in signal
+# y = df
+# perform FFT on signal
+fq = packet_rate_fft.packet_frequency()
+yf = fft(fq)
+# create new x-axis: frequency from signal
 xf = np.linspace(0.0, 1.0/(2.0*T), N//2)
-#plot results
+# plot results
 plt.plot(xf, yf[0:N//2], label = 'signal')
 plt.grid()
-plt.xlabel('Frequency (days)')
+plt.xlabel('Frequency (secs)')
 plt.ylabel(r'Spectral Amplitude')
 plt.legend(loc=1)
-plt.savefig('rsam_2016_2017_snippet_fft_firstresult.jpg')
+plt.savefig('packet_fft.jpg')
 plt.show()
--- packet_rate_fft.py
+++ packet_rate_fft.py
@@ -1,27 +1,26 @@
 import csv 
 import pandas as pd
 
+def packet_frequency():
+    addr = "minzhao.csv"
+    data = pd.read_csv(addr)
 
+    packet_count_list = []
+    packet_count = 0
+    start = 0
+    currTime = 0
+    for index, row in data.iterrows():
+        if row['TIME'] != currTime and start == 1:
+            packet_count_list.append(packet_count)
+            packet_count = 0
+            currTime += 1
 
-addr = "minzhao.csv"
-data = pd.read_csv(addr)
+        if start == 0:
+            start = 1
+            currTime = row['TIME']
 
-packet_count_list = []
-packet_count = 0
-start = 0
-currTime = 0
-for index, row in data.iterrows():
-    if row['TIME'] != currTime and start == 1:
-        packet_count_list.append(packet_count)
-        packet_count = 0
-        currTime += 1
+        packet_count += 1
 
-    if start == 0:
-        start = 1  
-        currTime = row['TIME'] 
-
-    packet_count += 1
-
-print(packet_count_list)
+    return packet_count_list
 
      
