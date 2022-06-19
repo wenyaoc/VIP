60fec3c7c9c5a9b3f75e654c38d1c70534475917
Shaoqian Zhou <zhouushaooqiann@Outlook.com>
2022-06-20 08:57:40 +1000

    IpStat class

--- src/read_pcap_dpkt.py
+++ src/read_pcap_dpkt.py
@@ -15,6 +15,107 @@ import time
 from numpy import outer
 
 
+class IpStat:
+    def __init__(self, target_IPs) -> None:
+        self.local_stat = {}
+        self.target_IPs = target_IPs
+
+    def update_stat(self, id, time, size, eth_src, eth_dst, ip_src, ip_dst, proto, port_src, port_dst):
+        for ip in self.target_IPs:
+            if (ip_src == ip or ip_dst == ip) and self.local_stat.get(ip) == None:
+                self.local_stat[ip] = {}
+                self.local_stat[ip]["incoming traffic"] = {}
+                self.local_stat[ip]["incoming traffic"]["#packet"] = 0
+                self.local_stat[ip]["incoming traffic"]["traffic in bytes"] = 0
+                self.local_stat[ip]["outgoing traffic"] = {}
+                self.local_stat[ip]["outgoing traffic"]["#packet"] = 0
+                self.local_stat[ip]["outgoing traffic"]["traffic in bytes"] = 0
+                self.local_stat[ip]["external IPs"] = {}
+                self.local_stat[ip]["internal ports"] = {}
+                self.local_stat[ip]["external ports"] = {}
+                self.local_stat[ip]["protocols"] = {}
+
+            if p_ip_src == ip:
+                # outgoing packet
+                self.local_stat[ip]["outgoing traffic"]["#packet"] += 1
+                self.local_stat[ip]["outgoing traffic"]["traffic in bytes"] += size
+                
+                if self.local_stat[ip]["external IPs"].get(ip_dst) == None:
+                    self.local_stat[ip]["external IPs"][ip_dst]["#incoming packet"] = 0
+                    self.local_stat[ip]["external IPs"][ip_dst]["incoming traffic in bytes"] = 0
+                    self.local_stat[ip]["external IPs"][ip_dst]["#outgoing packet"] = 0
+                    self.local_stat[ip]["external IPs"][ip_dst]["outgoing traffic in bytes"] = 0
+                
+                if self.local_stat[ip]["internal ports"].get(port_src) == None:
+                    self.local_stat[ip]["internal ports"][port_src]["#packet"] = 0
+                    self.local_stat[ip]["internal ports"][port_src]["traffic in bytes"] = 0
+
+                if self.local_stat[ip]["external ports"].get(port_dst) == None:
+                    self.local_stat[ip]["external ports"][port_dst]["#packet"] = 0
+                    self.local_stat[ip]["external ports"][port_dst]["traffic in bytes"] = 0
+
+                if self.local_stat[ip]["protocols"].get(proto) == None:
+                    self.local_stat[ip]["protocols"][proto] = {}
+                    self.local_stat[ip]["protocols"][proto]["#packet"] = 0
+                    self.local_stat[ip]["protocols"][proto]["traffic in bytes"] = 0
+
+                # update info
+                self.local_stat[ip]["external IPs"][ip_dst]["#outgoing packet"] += 1
+                self.local_stat[ip]["external IPs"][ip_dst]["outgoing traffic in bytes"] += size
+
+                self.local_stat[ip]["internal ports"][port_src]["#packet"] += 1
+                self.local_stat[ip]["internal ports"][port_src]["traffic in bytes"] += size
+                self.local_stat[ip]["external ports"][port_dst]["#packet"] += 1
+                self.local_stat[ip]["external ports"][port_dst]["traffic in bytes"] += size
+
+                self.local_stat[ip]["protocols"][proto]["#packet"] += 1
+                self.local_stat[ip]["protocols"][proto]["traffic in bytes"] += size
+
+
+            elif ip_dst == ip:
+                # incoming packet
+                self.local_stat[ip]["incoming traffic"]["#packet"] += 1
+                self.local_stat[ip]["incoming traffic"]["traffic in bytes"] += size
+                
+                if self.local_stat[ip]["external ips"].get(ip_src) == None:
+                    self.local_stat[ip]["external IPs"][ip_src]["#incoming packet"] = 0
+                    self.local_stat[ip]["external IPs"][ip_src]["incoming traffic in bytes"] = 0
+                    self.local_stat[ip]["external IPs"][ip_src]["#incoming packet"] = 0
+                    self.local_stat[ip]["external IPs"][ip_src]["incoming traffic in bytes"] = 0
+
+                if self.local_stat[ip]["internal ports"].get(port_dst) == None:
+                    self.local_stat[ip]["internal ports"][port_dst]["#packet"] = 0
+                    self.local_stat[ip]["internal ports"][port_dst]["traffic in bytes"] = 0
+
+                if self.local_stat[ip]["external ports"].get(port_src) == None:
+                    self.local_stat[ip]["external ports"][port_src]["#packet"] = 0
+                    self.local_stat[ip]["external ports"][port_src]["traffic in bytes"] = 0
+                
+                if self.local_stat[ip]["protocols"].get(proto) == None:
+                    self.local_stat[ip]["protocols"][proto] = {}
+                    self.local_stat[ip]["protocols"][proto]["#packet"] = 0
+                    self.local_stat[ip]["protocols"][proto]["traffic in bytes"] = 0
+
+                # update info
+                self.local_stat[ip]["external IPs"][ip_src]["#incoming packet"] += 1
+                self.local_stat[ip]["external IPs"][ip_src]["incoming traffic in bytes"] += size
+
+                self.local_stat[ip]["internal ports"][port_dst]["#packet"] += 1
+                self.local_stat[ip]["internal ports"][port_dst]["traffic in bytes"] += size
+                self.local_stat[ip]["external ports"][port_src]["#packet"] += 1
+                self.local_stat[ip]["external ports"][port_src]["traffic in bytes"] += size
+
+                self.local_stat[ip]["protocols"][proto]["#packet"] += 1
+                self.local_stat[ip]["protocols"][proto]["traffic in bytes"] += size    
+
+
+    def get_packet_rate():
+        return
+
+    def get_byte_rate():
+        
+
+
 
 def mac_addr(address):
     """Convert a MAC address to a readable/printable string
@@ -322,150 +423,150 @@ def update_local_database():
             packet_stat[ip]["protocols"][p_proto]["#packet"] += 1
             packet_stat[ip]["protocols"][p_proto]["traffic in bytes"] += p_size
 
-def get_attr_for_ip(ip_addr):
-    """Get a pre-defined set of attributes for the given IP address
-    list of attribute:
-    Categorical:
-        port being used and ranks (by activity - pkt, vol, etc)
-        network protocols
-        transport protocols
-        application protocols
-        packet types and their distributions (e.g., TCP flags - SYN, ACK, SYN-ACK etc)
-        packet content features (e.g., TCP window size for possible retransimission)
+# def get_attr_for_ip(ip_addr):
+#     """Get a pre-defined set of attributes for the given IP address
+#     list of attribute:
+#     Categorical:
+#         port being used and ranks (by activity - pkt, vol, etc)
+#         network protocols
+#         transport protocols
+#         application protocols
+#         packet types and their distributions (e.g., TCP flags - SYN, ACK, SYN-ACK etc)
+#         packet content features (e.g., TCP window size for possible retransimission)
 
-    Quantitative attribute
-        packet count/volume to each port
-        difference between inbound and outbound packet/volume of entire/certain protocol
-        variance/periodicity of traffic (pkt, volume, etc) from different time of a host
-        packet length (in/out, protocols, avg, variance, etc)
-        (port, pkt, volume, transport type, duration, geographical, registry type, etc) distribution of external IPs/flows contacting a certain internal host
+#     Quantitative attribute
+#         packet count/volume to each port
+#         difference between inbound and outbound packet/volume of entire/certain protocol
+#         variance/periodicity of traffic (pkt, volume, etc) from different time of a host
+#         packet length (in/out, protocols, avg, variance, etc)
+#         (port, pkt, volume, transport type, duration, geographical, registry type, etc) distribution of external IPs/flows contacting a certain internal host
 
-        Args:
-            ip_addr
-        Returns:
+#         Args:
+#             ip_addr
+#         Returns:
 
-    """
+#     """
 
-    # TCP/UDP ports and corresponding packet count/volume
-    filtered_packets = []
-    #packet filtering
-    for packet in packets:
-        if packet["IP.src"] == ip_addr or packet["IP.dst"] == ip_addr:
-            filtered_packets.append(packet)
+#     # TCP/UDP ports and corresponding packet count/volume
+#     filtered_packets = []
+#     #packet filtering
+#     for packet in packets:
+#         if packet["IP.src"] == ip_addr or packet["IP.dst"] == ip_addr:
+#             filtered_packets.append(packet)
     
-    # port statistic
+#     # port statistic
 
-    total_size_in = 0
-    packet_in = 0
-    total_size_out = 0
-    packet_out = 0
-    ip_external_in = {}
-    ip_external_out = {}
-    ip_external_in_volume = {}
-    ip_external_out_volume = {}
-    port_in = {}
-    port_out = {}
-    port_in_volume = {}
-    port_out_volume = {}
-    currTime = filtered_packets[0]['TIME']
-    for packet in filtered_packets:
-        if (packet['TIME'] >= (currTime + 60)):
+#     total_size_in = 0
+#     packet_in = 0
+#     total_size_out = 0
+#     packet_out = 0
+#     ip_external_in = {}
+#     ip_external_out = {}
+#     ip_external_in_volume = {}
+#     ip_external_out_volume = {}
+#     port_in = {}
+#     port_out = {}
+#     port_in_volume = {}
+#     port_out_volume = {}
+#     currTime = filtered_packets[0]['TIME']
+#     for packet in filtered_packets:
+#         if (packet['TIME'] >= (currTime + 60)):
 
-            ip_external_in_occurrences = collections.Counter(ip_external_in)
-            ip_external_in_occurrences_volume = collections.Counter(ip_external_in_volume)
-            ranked_ip_external_in = ip_external_in_occurrences.most_common()
-            ranked_ip_external_in_volume = ip_external_in_occurrences_volume.most_common()
+#             ip_external_in_occurrences = collections.Counter(ip_external_in)
+#             ip_external_in_occurrences_volume = collections.Counter(ip_external_in_volume)
+#             ranked_ip_external_in = ip_external_in_occurrences.most_common()
+#             ranked_ip_external_in_volume = ip_external_in_occurrences_volume.most_common()
             
-            ip_external_out_occurrences = collections.Counter(ip_external_out)
-            ip_external_out_occurrences_volume = collections.Counter(ip_external_out_volume)
-            ranked_ip_external_out = ip_external_out_occurrences.most_common()
-            ranked_ip_external_out_volume = ip_external_out_occurrences_volume.most_common()
+#             ip_external_out_occurrences = collections.Counter(ip_external_out)
+#             ip_external_out_occurrences_volume = collections.Counter(ip_external_out_volume)
+#             ranked_ip_external_out = ip_external_out_occurrences.most_common()
+#             ranked_ip_external_out_volume = ip_external_out_occurrences_volume.most_common()
 
-            port_in_occurrences = collections.Counter(port_in)
-            port_in_occurrences_volume = collections.Counter(port_in_occurrences)
-            ranked_port_in = port_in_occurrences.most_common()
-            ranked_port_in_volume = port_in_occurrences_volume.most_common()
+#             port_in_occurrences = collections.Counter(port_in)
+#             port_in_occurrences_volume = collections.Counter(port_in_occurrences)
+#             ranked_port_in = port_in_occurrences.most_common()
+#             ranked_port_in_volume = port_in_occurrences_volume.most_common()
 
-            port_out_occurrences = collections.Counter(port_out)
-            port_out_occurrences_volume = collections.Counter(port_out_occurrences)
-            ranked_port_out = port_out_occurrences.most_common()
-            ranked_port_out_volume = port_out_occurrences_volume.most_common()
+#             port_out_occurrences = collections.Counter(port_out)
+#             port_out_occurrences_volume = collections.Counter(port_out_occurrences)
+#             ranked_port_out = port_out_occurrences.most_common()
+#             ranked_port_out_volume = port_out_occurrences_volume.most_common()
 
-            print(f"time {datetime.datetime.fromtimestamp(currTime).strftime('%Y-%m-%d %H:%M:%S')}")
+#             print(f"time {datetime.datetime.fromtimestamp(currTime).strftime('%Y-%m-%d %H:%M:%S')}")
 
-            print(f"total packet number (in): {packet_in}")
-            print(f"Avg size (in): {total_size_in/packet_in}")
+#             print(f"total packet number (in): {packet_in}")
+#             print(f"Avg size (in): {total_size_in/packet_in}")
 
-            print(f"total packet number (out): {packet_out}")
-            print(f"Avg size (out): {total_size_out/packet_out}")
+#             print(f"total packet number (out): {packet_out}")
+#             print(f"Avg size (out): {total_size_out/packet_out}")
 
-            print(f"# incoming IP: {len(ip_external_in_occurrences)}")
-            print(f"# outgoing IP: {len(ip_external_out_occurrences)}")
+#             print(f"# incoming IP: {len(ip_external_in_occurrences)}")
+#             print(f"# outgoing IP: {len(ip_external_out_occurrences)}")
 
-            print("port_in_occurrences:")
-            print(port_in_occurrences)
-            print("port_out_occurrences:")
-            print(port_out_occurrences)
+#             print("port_in_occurrences:")
+#             print(port_in_occurrences)
+#             print("port_out_occurrences:")
+#             print(port_out_occurrences)
 
-            plot_piechart(ip_external_in_occurrences, f'./output/dpkt/myunsw23_ip_{currTime}_in_by_packet_number.jpg', 700)
-            plot_piechart(ip_external_out_occurrences, f'./output/dpkt/myunsw23_ip_{currTime}_out.jpg', 700)
-            plot_piechart(ip_external_in_occurrences_volume, f'./output/dpkt/myunsw23_ip_{currTime}_in_by_traffic_volume', 50000)
-            plot_piechart(ip_external_out_occurrences_volume, f'./output/dpkt/myunsw23_ip_{currTime}_in_by_traffic_volume', 50000)
-            #plot_piechart(port_in_occurrences, f'myunsw23_port_in.jpg', 0)
-            #plot_piechart(port_out_occurrences, f'myunsw23_port_out.jpg', 0)
+#             plot_piechart(ip_external_in_occurrences, f'./output/dpkt/myunsw23_ip_{currTime}_in_by_packet_number.jpg', 700)
+#             plot_piechart(ip_external_out_occurrences, f'./output/dpkt/myunsw23_ip_{currTime}_out.jpg', 700)
+#             plot_piechart(ip_external_in_occurrences_volume, f'./output/dpkt/myunsw23_ip_{currTime}_in_by_traffic_volume', 50000)
+#             plot_piechart(ip_external_out_occurrences_volume, f'./output/dpkt/myunsw23_ip_{currTime}_in_by_traffic_volume', 50000)
+#             #plot_piechart(port_in_occurrences, f'myunsw23_port_in.jpg', 0)
+#             #plot_piechart(port_out_occurrences, f'myunsw23_port_out.jpg', 0)
 
-            total_size_in = 0
-            packet_in = 0
-            total_size_out = 0
-            packet_out = 0
-            ip_external_in.clear()
-            ip_external_out.clear()
-            port_in.clear()
-            port_out.clear()
-            currTime = packet['TIME']
+#             total_size_in = 0
+#             packet_in = 0
+#             total_size_out = 0
+#             packet_out = 0
+#             ip_external_in.clear()
+#             ip_external_out.clear()
+#             port_in.clear()
+#             port_out.clear()
+#             currTime = packet['TIME']
 
 
-        else:
-            # outgoing packet
-            if packet['IP.src'] == ip_addr:
-                # update external ip address bookkeeping
-                if ip_external_out.get(packet['IP.dst']) == None:
-                    ip_external_out[packet['IP.dst']] = 1
-                    ip_external_out_volume[packet['IP.dst']] = packet['Size']
-                else:
-                    ip_external_out[packet['IP.dst']] += 1
-                    ip_external_out_volume[packet['IP.dst']] += packet['Size']
-                # update port number bookkeeping
-                if port_out.get(packet['port.src']) == None:
-                    port_out[packet['port.src']] = 1
-                    port_out_volume[packet['port.src']] = packet['Size']
-                else:
-                    port_out[packet['port.src']] += 1
-                    port_out_volume[packet['port.src']] += packet['Size']
+#         else:
+#             # outgoing packet
+#             if packet['IP.src'] == ip_addr:
+#                 # update external ip address bookkeeping
+#                 if ip_external_out.get(packet['IP.dst']) == None:
+#                     ip_external_out[packet['IP.dst']] = 1
+#                     ip_external_out_volume[packet['IP.dst']] = packet['Size']
+#                 else:
+#                     ip_external_out[packet['IP.dst']] += 1
+#                     ip_external_out_volume[packet['IP.dst']] += packet['Size']
+#                 # update port number bookkeeping
+#                 if port_out.get(packet['port.src']) == None:
+#                     port_out[packet['port.src']] = 1
+#                     port_out_volume[packet['port.src']] = packet['Size']
+#                 else:
+#                     port_out[packet['port.src']] += 1
+#                     port_out_volume[packet['port.src']] += packet['Size']
                 
-                total_size_out += packet['Size']
+#                 total_size_out += packet['Size']
 
-                packet_out += 1
+#                 packet_out += 1
 
-            # incoming packet
-            else:
-                # update external ip address bookkeeping
-                if ip_external_in.get(packet['IP.src']) == None:
-                    ip_external_in[packet['IP.src']] = 1
-                    ip_external_in_volume[packet['IP.src']] = packet['Size']
-                else:
-                    ip_external_in[packet['IP.src']] += 1
-                    ip_external_in_volume[packet['IP.src']] += packet['Size']
-                # update port number bookkeeping
-                if port_in.get(packet['port.dst']) == None:
-                    port_in[packet['port.dst']] = 1
-                    port_in_volume[packet['port.dst']] = packet['Size']
-                else:
-                    port_in[packet['port.dst']] += 1 
-                    port_in_volume[packet['port.dst']] += packet['Size']
+#             # incoming packet
+#             else:
+#                 # update external ip address bookkeeping
+#                 if ip_external_in.get(packet['IP.src']) == None:
+#                     ip_external_in[packet['IP.src']] = 1
+#                     ip_external_in_volume[packet['IP.src']] = packet['Size']
+#                 else:
+#                     ip_external_in[packet['IP.src']] += 1
+#                     ip_external_in_volume[packet['IP.src']] += packet['Size']
+#                 # update port number bookkeeping
+#                 if port_in.get(packet['port.dst']) == None:
+#                     port_in[packet['port.dst']] = 1
+#                     port_in_volume[packet['port.dst']] = packet['Size']
+#                 else:
+#                     port_in[packet['port.dst']] += 1 
+#                     port_in_volume[packet['port.dst']] += packet['Size']
                 
-                total_size_in += packet['Size']
-                packet_in += 1
+#                 total_size_in += packet['Size']
+#                 packet_in += 1
 
 
 start_time = datetime.now()
@@ -495,7 +596,8 @@ p_proto = None
 p_port_src = None
 p_port_dst = None
 
-packet_stat = {}
+# create local database
+local_database = IpStat(target_ips)
 
 #header = ['Packet ID', 'TIME', 'Size', 'eth.src', 'eth.dst', 'IP.src', 'IP.dst', 'IP.proto', 'port.src', 'port.dst']
 
@@ -552,7 +654,7 @@ for timestamp, buf in pcap:
     p_port_src = srcport
     p_port_dst = dstport
 
-    update_local_database()
+    local_database.update_stat(p_id, p_time, p_size, p_eth_src, p_eth_dst, p_ip_src, p_ip_dst, p_proto, p_port_src, p_port_dst)
     
 
 f.close()
