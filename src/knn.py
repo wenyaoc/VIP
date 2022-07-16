82fb8de19ff51c5aea7b6097ddeddde7f565fee1
Wenyao Chen <67650151+wenyaoc@users.noreply.github.com>
2022-07-16 22:15:59 +1000

    Add files via upload

--- src/knn.py
+++ src/knn.py
@@ -5,39 +5,39 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder  
 from sklearn import metrics
+import numpy as np
 
-
+from collections import defaultdict
 
 path_addr = "/home/wenyao/21Feb.csv"
 df = pd.read_csv (path_addr)
 # df['host type'] = df['host type'].astype(int)
-X = df.iloc[:,4:]
+# X = df.iloc[:,4:]
+# index = np.r_[4:8, 12:13, 15, 19]
+df['top proto(pkt)'] = pd.factorize(df['top proto(pkt)'])[0].astype(np.uint16)
+df['top proto(byte)'] = pd.factorize(df['top proto(byte)'])[0].astype(np.uint16)
+print(df.head())
+X = df.iloc[:, 4:]
 Y = df.iloc[:,1]
-# print(df.head())
-# print(X.head())
-# print(Y)
-# le = LabelEncoder()
-# le.fit_transform(Y)
 
-# print(Y)
 
 train_X, test_X, train_Y, test_Y = train_test_split(X, Y, test_size = 0.3)
 
-le = LabelEncoder()
-le.fit(train_Y)
-le.transform(train_Y)
-le.transform(test_Y)
+# le = LabelEncoder()
+# le.fit(train_Y)
+# le.transform(train_Y)
+# le.transform(test_Y)
 # print(train_Y)
 # print(test_Y)
 #Create KNN Classifier
-knn = KNeighborsClassifier(n_neighbors=6)
+# knn = KNeighborsClassifier(n_neighbors=6)
 
-#Train the model using the training sets
-knn.fit(train_X, train_Y)
+# #Train the model using the training sets
+# knn.fit(train_X, train_Y)
 
-#Predict the response for test dataset
-y_pred = knn.predict(test_X)
-print("KNN Accuracy:",metrics.accuracy_score(test_Y, y_pred))
+# #Predict the response for test dataset
+# y_pred = knn.predict(test_X)
+# print("KNN Accuracy:",metrics.accuracy_score(test_Y, y_pred))
 
 
 
